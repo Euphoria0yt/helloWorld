1dc6670042beb95c3d4f5660232799b1c5d6cac3
yt <1431743135@qq.com>
2023-09-11 00:48:28 +0800

    添加hello测试接口

--- src/main/java/com/yt/controller/HelloController.java
+++ src/main/java/com/yt/controller/HelloController.java
@@ -1,2 +1,15 @@
-package com.yt.controller;public class HelloController {
+package com.yt.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class HelloController {
+    @GetMapping("")
+    public String hello(){
+        System.out.println("hello world...");
+        return "OK";
+    }
 }
